e56234c22b7891dfcbf37fa6bf13f025b076775e
Brynner Chavez Gomez <anderson186159@gmail.com>
2024-10-03 11:22:58 -0400

    Chapter 10.4j

--- bc-fitness10/scripts/script.js
+++ bc-fitness10/scripts/script.js
@@ -4,3 +4,17 @@ Script.js
 10/3/24
 */
 
+//hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    var logo = document.getElementById("ffc-logo");
+    if (menu.style.display === "block" && logo.style.display ==="none") {
+        menu.style.display = "none";
+        logo.style.display = "block";
+    }
+    else {
+        menu.style.display = "block";
+        logo.style.display = "none";
+    }
+
+}
