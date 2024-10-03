59b4d9db094fb55338238c971f3c531129f4be96
Brynner Chavez Gomez <anderson186159@gmail.com>
2024-10-03 13:02:02 -0400

    Chapter 10-4m  and added media

--- bc-fitness10/scripts/script.js
+++ bc-fitness10/scripts/script.js
@@ -4,6 +4,11 @@ Script.js
 10/3/24
 */
 
+// Global variables
+var video = document.getElementById("example");
+var videoSource = document.getElementById("vid-src");
+var descriptionSource = document.getElementById("despsrc");
+
 //hamburger menu function
 function hamburger() {
     var menu = document.getElementById("menu-links");
@@ -16,5 +21,12 @@ function hamburger() {
         menu.style.display = "block";
         logo.style.display = "none";
     }
-
 }
+
+    //Function to display the burpees example video
+    function burpees() {
+        videoSource.src = "media/burpees.mp4";
+        descriptionSource.src = "media/burpees-description.vtt";
+        video.style.display = "block";
+        video.load();
+    }
